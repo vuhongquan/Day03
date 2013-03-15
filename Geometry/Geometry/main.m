61edd2ca21502d82cc7c1f96ed43dc5e6a943e14
thanhdao <thanhdd1@gmail.com>
2013-03-15 10:34:20 +0700

    Bai tap day03
    geometry

---
+++ Geometry/Geometry/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Geometry
+//
+//  Created by iOS12 on 3/15/13.
+//  Copyright (c) 2013 TechMaster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
